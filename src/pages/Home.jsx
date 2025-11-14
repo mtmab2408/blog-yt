8d63dd0e162f9edb19200ec67d68c1dd444eeaa6
Muhammad TALHA <mtmab2408@gmail.com>
2025-11-14 19:26:01 +0100

    feat: add BlogList component with tag-based filtering and JSON data fetching

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,13 +1,18 @@
 import React from 'react'
 import Header from '../components/Header'
 import Search from '../components/Search'
+import BlogList from '../components/BlogList';
+import { useState } from "react"
 function Home() {
+   const [selectedTag, setSelectedTag] = useState("All");
   return (
     <div>
       {/* Header */}
       <Header/>
       {/* Search */}
-      <Search/>
+       <Search onTagChange={(tag) => setSelectedTag(tag)} />
+      {/* BlogList */}
+      <BlogList selectedTag={selectedTag} />
     </div>
   )
 }
